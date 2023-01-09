9c005b36e7e2225bf035eef4f5fa28fbb51ad4e6
Sean Howe <seanmarchowe@gmail.com>
2023-01-09 14:58:26 +0000

    Error deploying with this file

--- medium-sanity/sanity.cli.ts
+++ medium-sanity/sanity.cli.ts
@@ -1,5 +1,5 @@
-import {defineCliConfig} from 'sanity/cli'
-
+import {defineCliConfig} from '@sanity/cli'
+// Error deploying with this file
 export default defineCliConfig({
   api: {
     projectId: '5ho3sdvi',
